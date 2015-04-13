913c2df9e8550d0227bdb4f1b63e612e0475ce7f
Kenny Huang <KHuangtai@gmail.com>
2015-04-13 02:00:38 -0400

    change tagging scope

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,5 +7,5 @@ class Project < ActiveRecord::Base
 	validates :author_id, :project_url, presence: true
 	acts_as_taggable
 	acts_as_taggable_on :project_name,
-	scope :by_join_date, order("created_at DESC")
+	
 end
